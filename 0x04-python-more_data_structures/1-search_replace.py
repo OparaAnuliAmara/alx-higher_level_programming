3dd2d3e049318703ed42eac82e2edae27b9978a5
OparaAnuliAmara <amaraopara33@gmail.com>
2022-09-28 10:32:22 -0700

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#a function that replaces all occurrences of an element by another in a new list
 def search_replace(my_list, search, replace):
     def s_r_elm(elm):
         return (elm if elm != search else replace)
