9e4b7882204a097329eedc6b121c72685ae9c184
OparaAnuliAmara <amaraopara33@gmail.com>
2022-12-20 19:54:40 -0800

    relationship_state.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
 """
 python file that contains the class definition of a
-State and an instance Base = declarative_base()
+Cities and an instance Base = declarative_base()
 """
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
-from relationship_city import City, Base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import ForeignKey
 
 
-class State(Base):
-    """State class"""
+Base = declarative_base()
 
-    __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+
+class City(Base):
+    """Cities class"""
+
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True,
+                nullable=False, autoincrement=True, unique=True)
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref="state",
-                          cascade="all, delete-orphan")
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
